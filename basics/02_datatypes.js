12450b18d8bff2cc0bebd9afe0dce51c9d5fb585
Mayank-png-dev <msingh13_be23@thapar.edu>
2025-10-20 18:05:22 +0000

    updated notes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)// we are using nodejs, not browser
+// ecma script --> Documentation for java script
+// mdnwebdocs
+
+let name = "Mayank"
+let age = 10
+let isloggesIn = false
+
+// number => range 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalon value //(null is object type)
+//undefined =>
+//symbol =>unique
+
+//objects
+ 
+console.log(typeof "age");
+
+// see specification on ecma script
