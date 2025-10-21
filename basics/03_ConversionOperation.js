a5f9f8ee6613f9ec690ae8e2eed59ca9432486ad
Mayank-png-dev <msingh13_be23@thapar.edu>
2025-10-21 05:24:43 +0000

    conversion of datatypes

---
+++ basics/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+//const {score} = req.body
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 3
+//"33abc" => NaN
+//true => 1; False => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "mayank"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
