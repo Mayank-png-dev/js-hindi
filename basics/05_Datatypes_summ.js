5e4a2a6da0304b1a346c21b7694870c406a1920c
Mayank-png-dev <msingh13_be23@thapar.edu>
2025-10-30 07:17:18 +0000

    upadated datatypes summary

---
+++ basics/05_Datatypes_summ.js
@@ -0,0 +1,28 @@
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTempp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId) //==>False
+
+const bigNumber = 3437384032499234n
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj={
+    name: "hitesh",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello wprld")
+} 
+
+//its important to knoe the typeof function u r dealing with
+console.log(typeof bigNumber) // datatype from typeof --> asked in interviews
