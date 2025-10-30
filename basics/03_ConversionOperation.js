102ebc30a0cfe47bbbfd5120eaf5da461e31ed8b
Mayank-png-dev <msingh13_be23@thapar.edu>
2025-10-30 06:21:15 +0000

    updated lectures

--- basics/03_ConversionOperation.js
+++ basics/03_ConversionOperation.js
@@ -23,4 +23,29 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+let str1 = "hello"
+let str2 = " Mayank"
+let str3 = str1 + str2;
+console.log(str3);
+
+//Complex situations
+///console.log("1"+2);
+///console.log(1 + "2");
+///console.log("1" + 2 + 2);
+///console.log(1 + 2 + "2");// look for defined instructions in echma script. we would see more later
+
+//Note: Always write code with an intension that the fello developer can read/understand it do not make ode comlex.
+
+//console.log(+true) => 1
+//console.log(+"") => 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100;
+//gamecounter++;
+++gamecounter;                  // study prefix and postfix docs from js mdn incremental
+console.log(gamecounter);
+
